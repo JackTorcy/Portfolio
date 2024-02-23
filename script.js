0746f1145d3be51a85834ea0f79c6bf314a830e7
JackTorcy <Jack@torcy.com>
2024-02-23 13:33:14 +0000

    Background Mostly complete
    Happy with the background now.

--- script.js
+++ script.js
@@ -1,54 +1,57 @@
-let ballColour = '#A3BBAD';
-let ballNum = 80;
-// ballSize = [size(px),speed(seconds)]
-let ballSize = [3, 5, 7];
-let ballSpeed = [15, 10, 5];
+//!variable declaration
+let ballNum = 250;
+let ballSize = 4;
 let balls = [];
 
+//!Generates balls
 for (let i = 0; i < ballNum; i++) {
-    let currentBallSize = ballSize[Math.floor(Math.random() * 3)]; // 3,5 or 7px WORKING
+    //Create balls
+    let ball = document.createElement('div'); // create div's
+    ball.classList.add('ball'); //add class
+    
+    //Ball Position
+    ball.style.top = `${Math.floor(Math.random() * 165)}vh`; //Random y-axis starting point //!Max Screen VH of 215
+    ball.style.left = `${Math.floor(Math.random() * 99)}vw`;//Random x-axis starting point
 
-    let ball = document.createElement('div'); // creates div WORKING
-    ball.classList.add('ball'); //give it ball class WORKING
-
-    ball.style.top = `${Math.floor(Math.random() * 215)}vh`; //Starting point as bottom of screen
-    ball.style.left = `${Math.floor(Math.random() * 99)}vw`;//Starting point random point on x axis
-
-    ball.style.height = `${currentBallSize}px`; //height as currentBall WORKING
-    ball.style.width = ball.style.height; //width=height WORKING
-
+    //Ball Size
+    ball.style.height = `${ballSize}px`; //ball height
+    ball.style.width = ball.style.height; //ball width - same as height
+    
+    //Puts balls on the screen
     balls.push(ball) // add ball to the balls array
-    document.body.append(ball)
+    document.body.append(ball) //Adds the ball to the body individually
 }
 
-
-
-console.log(balls)
-
+//!Gives balls movement
 for (let i = 0; i < balls.length; i++) {
-    let currentBall = balls[i]
-    startX = Number(currentBall.style.left.replace('vw', ''))
-    startY = Number(currentBall.style.top.replace('vh', ''))
+    //Defines current iteration
+    let currentBall = balls[i] 
 
+    //Random value in range generator
     function randomValue(min, max) {
         return Math.floor(Math.random() * (max - min + 1) + min)
     }
 
+    //!Animation Variables
+    //First movement
+    transformx1 = randomValue(-10, 10)
+    transformy1 = randomValue(-10, 10)
+    //Second movement
+    transformx2 = randomValue(-10, 10)
+    transformy2 = randomValue(-10, 10)
+    //Speed - between 20-30s for full animation
+    animationLen = randomValue(20000,30000)
+
+
     currentBall.animate([
-        { transform: `translate(0px,0px)` },
-        { transform: `translate(${randomValue(-10, 10)}vw,${randomValue(-10, 10)}vh)` },
-        { transform: `translate(${randomValue(-10, 10)}vw,${randomValue(-10, 10)}vh)` }
+        {transform: `translate(0px,0px)`,easing:'ease-in'},
+        {transform: `translate(${transformx1}vw,${transformy1}vh)`,easing:'linear'},
+        {transform: `translate(${transformx2}vw,${transformy2}vh)`,easing:'linear'},
+        {transform: `translate(0px,0px)`, easing:'ease-out'}
+
     ], {
-        duration: 5000,
-        iterations: Infinity
+        duration: animationLen,
+        iterations: Infinity,
     });;
-
-
-    /*
-    For random directions:
-    -Take starting positions as posX and posY
-    -Randomly select 2 new positions within 10vh and 10vw
-    -Have ballMovement go to each of the positions, repeating infinity with random speed, particles will be slowly floating around
-    */
 }
 
