ee5a15003c2e64db9e08e98e55a89a85076ec38d
JackTorcy <Jack@torcy.com>
2024-02-23 11:52:59 +0000

    Saving progress on background
    Fully changed method of doing background

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let ballColour = '#A3BBAD';
-let ballNum = 68;
+let ballNum = 80;
 // ballSize = [size(px),speed(seconds)]
 let ballSize = [3, 5, 7];
 let ballSpeed = [15, 10, 5];
@@ -27,64 +27,28 @@ console.log(balls)
 
 for (let i = 0; i < balls.length; i++) {
     let currentBall = balls[i]
-    // console.log(currentBall)
-    let currentBallSpeed = 0
-    // console.log('complete')
-    if (currentBall.style.height == '3px') {
-        currentBallSpeed = ballSpeed[0]
-    } else if (currentBall.style.height == '5px') {
-        currentBallSpeed = ballSpeed[1]
-    } else {
-        currentBallSpeed = ballSpeed[2]
+    startX = Number(currentBall.style.left.replace('vw', ''))
+    startY = Number(currentBall.style.top.replace('vh', ''))
+
+    function randomValue(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min)
     }
 
-    // let id = null;
-    // let posY = currentBall.style.top
-    // posY = Number(posY.replace('vh', ''))
-    // let posX = currentBall.style.left
-    // posX = Number(posX.replace('vw', ''))
-    // clearInterval(id);
-    // id = setInterval(movement, 2000);
-    // function movement() {
-    //     currentBall.style.top = posY + 40 + 'vh'
-    //     currentBall.style.left = posX + 40 + 'vw'
-    // }
-    function randomTiming(min,max) {
-        return Math.floor(Math.random() * (max-min+1)+min)
-    }
+    currentBall.animate([
+        { transform: `translate(0px,0px)` },
+        { transform: `translate(${randomValue(-10, 10)}vw,${randomValue(-10, 10)}vh)` },
+        { transform: `translate(${randomValue(-10, 10)}vw,${randomValue(-10, 10)}vh)` }
+    ], {
+        duration: 5000,
+        iterations: Infinity
+    });;
 
-
-    const ballMovement = [
-        { transform: "translateX(5vw)" },
-        { transform: "translateX(80vw)" },
-    ];
-
-    const ballTiming = {
-        duration: randomTiming(5000,15000),
-        iterations: Infinity,
-    };
-
-    currentBall.animate(ballMovement, ballTiming);
 
     /*
     For random directions:
     -Take starting positions as posX and posY
     -Randomly select 2 new positions within 10vh and 10vw
     -Have ballMovement go to each of the positions, repeating infinity with random speed, particles will be slowly floating around
-    
     */
-
-    // currentBall.animate(
-    //     [
-    //         { transform: "scale(0)" },
-    //         { transform: "scale(1)" },
-    //     ],
-    //     {
-    //         duration: 5,
-    //         iterations: 1,
-
-    //     }
-
-    // )
 }
 
