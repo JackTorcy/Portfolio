9d409d459f8329edb9fb2a215915cbdca8f703b7
JackTorcy <Jack@torcy.com>
2024-03-24 21:33:46 +0000

    All JS comments added and complete

--- script.js
+++ script.js
@@ -1,86 +1,89 @@
-//!variable declaration
-let ballNum = 300;
-let ballSize = 4;
-let balls = [];
+//? variable declaration ?//
+let ballNum = 300; //No of background balls
+let ballSize = 4; //px size of balls
+let balls = []; //Empty array - balls to be added
 
-//!Generates balls
+//? Generates balls ?//
 for (let i = 0; i < ballNum; i++) {
-    //Create balls
-    let ball = document.createElement('div'); // create div's
-    ball.classList.add('ball'); //add class
+    //* Create balls *//
+    let ball = document.createElement('div'); //for each ball - creates div
+    ball.classList.add('ball'); //adds class to each ball - border-radius, position etc
 
-    //Ball Position
-    ball.style.top = `${Math.floor(Math.random() * 350)}vh`; //Random y-axis starting point //!Max Screen VH of 350
-    ball.style.left = `${Math.floor(Math.random() * 99)}vw`;//Random x-axis starting point
+    //* Ball Position *//
+    ball.style.top = `${Math.floor(Math.random() * 340)}vh`; //Random y-axis starting point //! Max Screen VH of 350 !//
+    ball.style.left = `${Math.floor(Math.random() * 99)}vw`; //Random x-axis starting point //! Max Screen VW of 100 !//
 
-    //Ball Size
-    ball.style.height = `${ballSize}px`; //ball height
-    ball.style.width = ball.style.height; //ball width - same as height
+    //* Ball Size *//
+    ball.style.height = `${ballSize}px`; //set height
+    ball.style.width = ball.style.height; //set width - same as height (circle)
 
-    //Puts balls on the screen
-    balls.push(ball) // add ball to the balls array
-    document.body.append(ball) //Adds the ball to the body individually
+    //* Puts balls on the screen *//
+    balls.push(ball) //Ball is setup, add to array
+    document.body.append(ball) //Adds each ball to the DOM
 }
 
-//!Gives balls movement
+//? Gives balls movement ?//
 for (let i = 0; i < balls.length; i++) {
-    //Defines current iteration
+    //* Local Variable - iteration purposes *//
     let currentBall = balls[i]
 
-    //Random value in range generator
+    //* Local Function - for random values *//
     function randomValue(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) + min)
+        return Math.floor(Math.random() * (max - min + 1) + min) //Returns a random value anywhere between 2 given values
     }
 
-    //!Animation Variables
-    //First movement
+    //? Variable Declaration - animation ?//
+    //* Random first point - calls local function *//
     transformx1 = randomValue(-10, 10)
     transformy1 = randomValue(-10, 10)
-    //Second movement
+    //* Random second point - calls local function *//
     transformx2 = randomValue(-10, 10)
     transformy2 = randomValue(-10, 10)
-    //Speed - between 20-30s for full animation
-    animationLen = randomValue(20000, 30000)
+    //* Speed - between 20-30s for full animation *//
+    animationLen = randomValue(20000, 30000) //Measured in milliseconds
 
-
+    //? Animation *//
     currentBall.animate([
+        //* Movement *//
         { transform: `translate(0px,0px)`, easing: 'ease-in' },
-        { transform: `translate(${transformx1}vw,${transformy1}vh)`, easing: 'linear' },
-        { transform: `translate(${transformx2}vw,${transformy2}vh)`, easing: 'linear' },
+        { transform: `translate(${transformx1}vw,${transformy1}vh)`, easing: 'linear' }, //Moves to first point - random value
+        { transform: `translate(${transformx2}vw,${transformy2}vh)`, easing: 'linear' }, //After, moves to second point - random value
         { transform: `translate(0px,0px)`, easing: 'ease-out' }
 
     ], {
+        //* Properties *//
         duration: animationLen,
         iterations: Infinity,
     });;
 }
-const about = document.querySelector('.about');
-const aboutObserver = new IntersectionObserver(entries => {
-    // Loop over the entries
-    entries.forEach(entry => {
-        // If the element is visible
-        if (entry.isIntersecting) {
-            // Add the animation class
-            about.classList.add('about-animation');
+
+//? Scroll Animation Triggers ?//
+//! ALL SAME - ONLY ADDED COMMENTS ON FIRST 2 !//
+//* About *//
+const about = document.querySelector('.about'); //Assigns HTML element to variable
+const aboutObserver = new IntersectionObserver(entries => { //Creates new Observer to watch intersection
+    entries.forEach(entry => { //Loop over the entries
+        if (entry.isIntersecting) { //If the element is visible
+            about.classList.add('about-animation'); //Add the animation class
             return;
         }
     });
 });
-aboutObserver.observe(about);
+aboutObserver.observe(about); //Calls about Observer - passing about variable
 
-const skillBox = document.querySelectorAll('.gridItem');
+//* Skills *//
+const skillBox = document.querySelectorAll('.gridItem'); //Assigns all HTML elements with class to variable
 const skillsObserver = new IntersectionObserver(entries => {
-    // Loop over the entries
     entries.forEach(entry => {
-        // If the element is visible
         if (entry.isIntersecting) {
-            // Add the animation class
-            entry.target.classList.add('skills-animation');
+            entry.target.classList.add('skills-animation'); //Assigns classlist to entry rather than specific element
         }
     });
 });
+//Iterate through all selected HTML elements and add class when on screen
 skillBox.forEach((element) => skillsObserver.observe(element));
 
+//* Project 1 *// 
 const project1 = document.querySelector('#card1');
 const project1Observer = new IntersectionObserver(entries => {
     // Loop over the entries
@@ -95,6 +98,7 @@ const project1Observer = new IntersectionObserver(entries => {
 });
 project1Observer.observe(project1)
 
+//* Project 2 *// 
 const project2 = document.querySelector('#card2');
 const project2Observer = new IntersectionObserver(entries => {
     // Loop over the entries
@@ -109,6 +113,7 @@ const project2Observer = new IntersectionObserver(entries => {
 });
 project2Observer.observe(project2)
 
+//* Project 3 *// 
 const project3 = document.querySelector('#card3');
 const project3Observer = new IntersectionObserver(entries => {
     // Loop over the entries
